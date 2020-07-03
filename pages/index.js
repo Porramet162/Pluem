c9998aea4b3dc21c2d50dffed3788a5b179e6149
Porramet162 <proramet38@gmail.com>
2020-07-03 22:08:07 +0700

    test

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Header from '../components/Header'
 export default () => {
   return (
     <div className="container">
-      <Header />fff
+      <Header />
 
 
       <div className="section">
